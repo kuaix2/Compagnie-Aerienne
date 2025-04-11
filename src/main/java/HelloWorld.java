fc3cea6cc9b112e2d19942d064049ef80a97a5d6
allan-benard <allandu97435@gmail.com>
2025-04-11 09:48:42 +0200

    test2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         // Correct usage of println
-        System.out.println("Hello");
+        System.out.println("Hellor");
     }
 }
